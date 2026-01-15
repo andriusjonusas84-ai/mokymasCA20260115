fd97d06623fa13b52c3737c3b8da16345be90ab6
Andrius Jonusas <andrius.jonusas84@gmail.com>
2026-01-15 20:26:25 +0200

    Patobulintas main.py

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ kursas = Kursas("Algis Stankevicius",40)
 python_kursas = PythonKursas("Donatas Noreikia",30)
 
 if __name__ == "__main__":
+    print("Ar mokymas vyksta")
     kursas.destyti()
     python_kursas.destyti()
